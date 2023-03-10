17f61401d88baeb93bb5ed7ed7b7c6f4ec6ae209
Christie Barron <christie.m.barron@gmail.com>
2023-03-10 07:47:13 -0500

    revised dynamic script

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,6 +6,9 @@ total_votes = 0
 candidates = []
 percent_votes = []
 candidate_count = []
+candidate_names = []
+candidate_votes = {}
+
 #winner = "a"
 
 #create a count of the candidate votes
@@ -29,41 +32,61 @@ with open (read_election, 'r') as f:
     
     for row in election:
         total_votes = total_votes + 1 #calculate the total number of votes
-        candidates.append(row[2]) #create list of all candidates
-
-        #calculate counts for each candidate
-        if (row[2] == "Charles Casper Stockham"):
-            ccs = ccs + 1
+        #candidates.append(row[2]) #create list of all candidates (repeated)
         
-        elif (row[2] == "Raymon Anthony Doane"):
-            rad = rad + 1
+        
+        #dictionary: if candidate not there, add it. if there, increment by one. 
 
-        elif (row[2] == "Diana DeGette"):
-            dd = dd + 1
+        candidate_names = row[2]
 
-        else:
-            print("typo or unexpected vote")
+        #
+        if (candidate_names not in candidates):
+            candidates.append(candidate_names)
 
-    candidates = set(candidates) #just keep unique values for candidates
+            #save candidate name as an index to the dictionary. Set candidate vote as zero.
+            candidate_votes[candidate_names] = 0
+        
+        #in the dictionary, index by the 'candidate name', add one to their vote count. 
+        candidate_votes[candidate_names] = candidate_votes[candidate_names] +1
+
+    print(candidates)
+    print(candidate_votes)
+
+        # #calculate counts for each candidate
+        # if (row[2] == "Charles Casper Stockham"):
+        #     ccs = ccs + 1
+        
+        # elif (row[2] == "Raymon Anthony Doane"):
+        #     rad = rad + 1
+
+        # elif (row[2] == "Diana DeGette"):
+        #     dd = dd + 1
+
+        # else:
+        #     print("typo or unexpected vote")
+
+    #candidates = set(candidates) #just keep unique values for candidates
 
     #calculate percentages
-    ccs_pct = (ccs / total_votes) * 100
-    dd_pct = (dd / total_votes) * 100
-    rad_pct = (rad / total_votes) * 100
+    # ccs_pct = (ccs / total_votes) * 100
+    # dd_pct = (dd / total_votes) * 100
+    # rad_pct = (rad / total_votes) * 100
 
-    #calculate winner
-    
+    # #calculate winner
+    # list[ccs, rad, dd]
+    # list[]
 
-    #printing the results
+    # #printing the results
     print("Election Results")
     print("-----------------------------")
     print(f"Total Votes: {total_votes}")
     print("-----------------------------")
-    print(f"Charles Casper Stockham: {ccs_pct}% ({ccs})")
-    print(f"Diana DeGette: {dd_pct}% ({dd})")
-    print(f"Raymon Anthony Doane: {rad_pct}% ({rad})")
-    print(f"-----------------------------")
-    print(f"Winner: ")
+    print()
+    # print(f"Charles Casper Stockham: {ccs_pct}% ({ccs})")
+    # print(f"Diana DeGette: {dd_pct}% ({dd})")
+    # print(f"Raymon Anthony Doane: {rad_pct}% ({rad})")
+    # print(f"-----------------------------")
+    # print(f"Winner: ")
 
     # print(total_votes)
     # print(set(candidates))
