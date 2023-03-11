e8a4dda4d78c7803760ed5b02059fe25fa31b7fc
Christie Barron <christie.m.barron@gmail.com>
2023-03-10 22:05:20 -0500

    revising result printing

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,12 @@
 import csv
 import os
+from contextlib import redirect_stdout #for writing to .txt file
+import sys
 
 #read path
 budget_read = "PyBank/resources/budget_data.csv"
 #write path
-budget_write = "PyBank/resources/buget_output.csv"
+budget_write = "PyBank/resources/buget_output.txt"
 
 #create the relevant variables
 date = []
@@ -12,6 +14,7 @@ profit = []
 net_total = 0
 profit_change = []
 sum_change = 0
+profit_dict = {}
 
 #read in the data
 with open (budget_read, 'r') as f:
@@ -26,6 +29,7 @@ with open (budget_read, 'r') as f:
         date.append(row[0])
         profit.append(int(row[1]))
         net_total = net_total + int(row[1])
+        profit_dict[row[0]] = row[1]
 
 #create a profit change list
     for i in range(len(profit) - 1):
@@ -36,17 +40,25 @@ with open (budget_read, 'r') as f:
         sum_change = sum_change + profit_change[i]
     average_change = sum_change / len(profit_change)
 
-    average_change.index
+    max(profit_change)
+    date
 
  #print the results
-    print("Financial Analysis")
-    print("------------------------")   
-    print(f"Total Months: {len(date)}")
-    print(f"Average Change: ${average_change}") 
-    print(f"Greatest Increase in Profits: {max(profit_change)}")
-    print(f"Greatest Decrease in Profits: {min(profit_change)}")
+    def print_output():
+        print("Financial Analysis")
+        print("------------------------")
+        print(f"Total Months: {len(date)}")
+        print(f"Total: {net_total}")
+        print(f"Average Change: ${average_change}") 
+        print(f"Greatest Increase in Profits: {date[profit_change.index(max(profit_change))+1]} {max(profit_change)}")
+        print(f"Greatest Decrease in Profits: {date[profit_change.index(min(profit_change))+1]} {min(profit_change)}")
 
-#NEED TO FIX THE INCREASE AND DECREASE IN PROFITS TO INCLUDE THE MONTH
-#NEED TO PRINT SCRIPT TO A TEXT FILE TOO. 
+    #print the output to terminal
+    print_output()
 
+    #print the results to text 
+    with open(budget_write, 'w') as f:
+        with redirect_stdout(f):
+            #code to print
+            print_output()
 
