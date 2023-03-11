de3374fe36141d23f9b8316dbfd95e61fcab92c5
Christie Barron <christie.m.barron@gmail.com>
2023-03-10 21:23:15 -0500

    made script dynamic

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,5 +1,9 @@
+#import dependencies
 import csv
 import os
+from contextlib import redirect_stdout #for writing to .txt file
+import sys
+
 
 #create variables
 total_votes = 0
@@ -9,21 +13,13 @@ candidate_count = []
 candidate_names = []
 candidate_votes = {}
 
-#winner = "a"
-
-#create a count of the candidate votes
-ccs = 0
-rad = 0
-dd = 0
-
 #readpath
 read_election = "PyPoll/resources/election_data.csv"
 
 #writepath
 write_election = "PyPoll/analysis/results.txt"
 
-
-
+#open file and save relevant data
 with open (read_election, 'r') as f:
     election = csv.reader(f, delimiter = ",")
 
@@ -31,13 +27,9 @@ with open (read_election, 'r') as f:
 
     
     for row in election:
-        total_votes = total_votes + 1 #calculate the total number of votes
-        #candidates.append(row[2]) #create list of all candidates (repeated)
-        
-        
-        #dictionary: if candidate not there, add it. if there, increment by one. 
+        total_votes = total_votes + 1 #calculate the total number of votes 
 
-        candidate_names = row[2]
+        candidate_names = row[2] #save the candidate name for each row
 
         #
         if (candidate_names not in candidates):
@@ -49,45 +41,35 @@ with open (read_election, 'r') as f:
         #in the dictionary, index by the 'candidate name', add one to their vote count. 
         candidate_votes[candidate_names] = candidate_votes[candidate_names] +1
 
-    print(candidates)
-    print(candidate_votes)
-
-        # #calculate counts for each candidate
-        # if (row[2] == "Charles Casper Stockham"):
-        #     ccs = ccs + 1
+    #calculate the percentage of votes for each candidate
+    for i in range(len(candidates)):
+        percent_votes.append(candidate_votes[candidates[i]]/total_votes)
         
-        # elif (row[2] == "Raymon Anthony Doane"):
-        #     rad = rad + 1
+    #create a function to make printing easier
+    def print_out(number):
+        print(f"{candidates[number]}: {percent_votes[number]}% ({candidate_votes[candidates[number]]})")
 
-        # elif (row[2] == "Diana DeGette"):
-        #     dd = dd + 1
-
-        # else:
-        #     print("typo or unexpected vote")
-
-    #candidates = set(candidates) #just keep unique values for candidates
-
-    #calculate percentages
-    # ccs_pct = (ccs / total_votes) * 100
-    # dd_pct = (dd / total_votes) * 100
-    # rad_pct = (rad / total_votes) * 100
-
-    # #calculate winner
-    # list[ccs, rad, dd]
-    # list[]
+    #create a printing function
+    def print_full_out():
 
     # #printing the results
-    print("Election Results")
-    print("-----------------------------")
-    print(f"Total Votes: {total_votes}")
-    print("-----------------------------")
-    print()
-    # print(f"Charles Casper Stockham: {ccs_pct}% ({ccs})")
-    # print(f"Diana DeGette: {dd_pct}% ({dd})")
-    # print(f"Raymon Anthony Doane: {rad_pct}% ({rad})")
-    # print(f"-----------------------------")
-    # print(f"Winner: ")
+        print("Election Results")
+        print("-----------------------------")
+        print(f"Total Votes: {total_votes}")
+        print("-----------------------------")
 
-    # print(total_votes)
-    # print(set(candidates))
-    # print(ccs, rad, dd)
+        #print out the remaining results
+        for i in range(len(candidates)):
+            print_out(i)
+
+        print("--------------------------")
+        print(f"Winner: {max(candidate_votes, key=lambda k: candidate_votes[k])}")
+
+    #print results to terminal    
+    print_full_out()
+
+    #save printed results to text file
+    with open(write_election, 'w') as f:
+        with redirect_stdout(f):
+            #code to print
+            print_full_out()
